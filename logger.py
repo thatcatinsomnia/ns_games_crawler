adb928c4d260fa93c40d21e25e6eb8b8e0f46a1d
thatcatinsomnia <thatcatinsomnia@gmail.com>
2019-03-10 17:55:44 +0800

    Logger to logging things

---
+++ logger.py
@@ -0,0 +1,26 @@
+import logging
+
+logfile = './log/app.log'
+
+# create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# create file handler and set level to debug
+fh = logging.FileHandler(logfile)
+fh.setLevel(logging.DEBUG)
+
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+# add formatter to ch and fh
+ch.setFormatter(formatter)
+fh.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+logger.addHandler(fh)
