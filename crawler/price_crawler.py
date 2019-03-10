d5e3244daf2d190386f971297ad132f6c48c051b
thatcatinsomnia <thatcatinsomnia@gmail.com>
2019-03-10 17:55:12 +0800

    Create price crawler

---
+++ crawler/price_crawler.py
@@ -0,0 +1,64 @@
+import requests, json
+from logger import logger
+
+class Price_Crawler:
+    def __init__(self, url):
+        self.url = url
+
+    def scrape_eshop_price(self):
+        response = requests.get(self.url)
+        if response.status_code == 200:
+            respjson = json.loads(response.text.encode('UTF-8'))
+            country = respjson.get('country')
+            prices = respjson.get('prices')
+            price_datas = self.filter_price_datas(prices)
+            logger.info(f'scrap {len(price_datas)} games price...')
+
+        return price_datas
+            
+    def filter_price_datas(self, prices):
+        price_datas = []
+        for price in prices:
+            title_id = price.get('title_id')
+            sales_status = price.get('sales_status')
+            amount = self.get_regular_amount(price)
+            discount_data = self.get_discount_amount_and_date(price)   
+            
+            price_data = {
+                'title_id': title_id,
+                'currency': amount.get('currency'),
+                'amount': amount.get('amount'),
+                'discount_amount': discount_data.get('discount_amount'),
+                'start_date': discount_data.get('discount_start_date'),
+                'end_date': discount_data.get('discount_end_date')
+            }
+            price_datas.append(price_data)
+        return price_datas
+
+    def get_regular_amount(self, price):
+        amount = 0
+        currency = '-'
+        if price.get('regular_price'):
+            regular_price = price.get('regular_price')
+            amount = regular_price.get('raw_value')
+            currency = regular_price.get('currency')
+
+        return {'amount': amount, 'currency': currency}
+
+    def get_discount_amount_and_date(self, price):
+        discount_amount = 0
+        discount_start_date = '-'
+        discount_end_date = '-'
+
+        if price.get('discount_price'):
+            discount_amount = price.get('discount_price').get('raw_value')
+            discount_start_date = price.get('discount_price').get('start_datetime')
+            discount_end_date = price.get('discount_price').get('end_datetime')
+
+        discount_amount_and_date = {
+            'discount_amount': discount_amount,
+            'discount_start_date': discount_start_date,
+            'discount_end_date': discount_end_date
+        }
+
+        return discount_amount_and_date
