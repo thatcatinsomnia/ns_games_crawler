4d1e73656f3c8ee85aa641affcaba937fda0b816
thatcatinsomnia <thatcatinsomnia@gmail.com>
2019-03-12 00:19:45 +0800

    change variable names

--- crawler/game_crawler.py
+++ crawler/game_crawler.py
@@ -6,28 +6,24 @@ class Game_Crawler():
     def __init__(self, url):
         self.url = url 
 
-    def scrape_games_data(self):
-        games_data = []
-        result = ''
+    def scrape_us_games_datas(self):
+        us_games_datas = []
         offset = 0
-        
         while True:
             url = f'{self.url}&offset={offset}'
             response = requests.get(url)
-
-            if response.status_code ==200:
+            if response.status_code == 200:
                 respjson = json.loads(response.text.encode('UTF-8'))
-                games = respjson.get('games').get('game')
-
-                if games:
-                    logger.info(f'Get {len(games)} games from offset {offset} ...')
-                    games_data.extend(games)
+                games_of_request = respjson.get('games').get('game')
+                if games_of_request:
+                    us_games_datas.extend(games_of_request)
+                    logger.info(f'Scrape {len(games_of_request)} games from offset {offset} ...')
                     offset += 200
                     sleep(1)
                 else:
-                    logger.info(f'No game found in offset {offset}')
+                    logger.info(f'No games in offset {offset}...')
                     break
             else:
-                logger.debug(f'CANT GET RESPONSE FROM {url}')
-        
-        return games_data
+                logger.info(f'Fail with resonpse code {response.status_code} in offset {offset}')
+            
+        return us_games_datas
