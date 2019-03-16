99c54a9f5163976228125f2f3a32118abe7b6ac9
thatcatinsomnia <thatcatinsomnia@gmail.com>
2019-03-16 23:59:31 +0800

    New method for request asia games info

--- crawler/game_crawler.py
+++ crawler/game_crawler.py
@@ -17,20 +17,17 @@ class Game_Crawler():
                 games_datas = respjson.get('games').get('game')
                 if games_datas:
                     us_games_datas += games_datas
-                    logger.info(f'Scrape games from Nintendo US offset = {offset}')
+                    logger.info(f'Request games from Nintendo US offset = {offset}')
                     offset += 200
                     sleep(1)
                 else:
-                    logger.info(f'No games in offset {offset}...')
+                    logger.info(f'No games in offset {offset} ==> STOP')
                     break
             else:
                 logger.info(f'Fail with resonpse code {response.status_code} in offset {offset}')
-            
         return us_games_datas
     
     def scrape_eu_games_datas(self):
-        
-        url = f'https://searching.nintendo-europe.com/en/select'
         params = {
             'fq': 'type:GAME AND dates_released_dts:[* TO NOW] AND system_type:nintendoswitch*',
             'q': '*',
@@ -39,8 +36,8 @@ class Game_Crawler():
             'start': 0,
             'rows': 9999
         }
-        logger.info(f'Scrape games from Nintendo EU with rows: {params.get("rows")}')
-        response = requests.get(url, params)
+        logger.info(f'Request games from Nintendo EU with rows: {params.get("rows")}')
+        response = requests.get(self.url, params)
 
         if response.status_code == 200:
             respjson = json.loads(response.text.encode('UTF-8'))
@@ -48,3 +45,31 @@ class Game_Crawler():
             return eu_games_datas
         else:
             logger.info(f'Error with response code: {response.status_code}')
+
+    def scrape_asia_games_datas(self):
+        page = 1
+        games_datas = []
+        while True:
+            params = {
+                'limit': 300,
+                'page': page,
+                'opt_hard':'1_HAC',
+                'opt_osale': 1,
+                'opt_ssitu': 'onsale'
+            }
+            response = requests.get(self.url, params)
+            if response.status_code == 200:
+                respjson = json.loads(response.text.encode('UTF-8'))
+                jp_games_datas = respjson.get('result').get('items')
+                if jp_games_datas:
+                    logger.info(f'Request games from Nintendo JP in page {page}')
+                    games_datas += jp_games_datas
+                    sleep(1)
+                else:
+                    logger.info(f'No games in page {page} ==> STOP')
+                    break
+            else:
+                logger.info(f'Fail with resonpse code {response.status_code} in page {page}')
+            page += 1
+
+        return games_datas
